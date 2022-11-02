0489a5d3f55db58684b347bddeab7a0f9e701ce1
Jose Carreño <Jcarreno4@gmail.com>
2022-11-02 14:37:48 -0500

    Core APU v2.0, tercera revision de modelo de insumos

--- src/java/ejbs/UsuarioFacade.java
+++ src/java/ejbs/UsuarioFacade.java
@@ -5,7 +5,6 @@
  */
 package ejbs;
 
-
 import entities.PgUsuario;
 import java.util.List;
 import java.util.logging.Logger;
@@ -23,99 +22,92 @@ import org.apache.commons.codec.digest.DigestUtils;
 @Stateless
 @ApplicationException(rollback = true)
 public class UsuarioFacade extends AbstractFacade<PgUsuario> {
+
     @PersistenceContext(unitName = "APUPU")
     EntityManager em;
 
-  public UsuarioFacade() {
+    public UsuarioFacade() {
         super(PgUsuario.class);
     }
-    
+
     protected EntityManager getEntityManager() {
         return em;
     }
-    
-    
 
     public List<PgUsuario> findUsuarioByUsuarioLogin(String usuario) {
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.usuario = :usuario");
         q.setParameter("usuario", usuario);
         return q.getResultList();
     }
-    
+
     public Long validLogin(String usuario, String password) {
-            String passwordCifrado = DigestUtils.md5Hex(password);
-            Query q = em.createQuery("SELECT j FROM PgUsuario j WHERE j.estadoregistro = 1 AND j.usuario = :usuario AND j.password = :password");
-            q.setParameter("usuario", usuario);
-            q.setParameter("password", passwordCifrado);
-            if (q.getResultList().size() == 1)
-            {
-                return 1L;
-            }
-            else
-            {
-                return 0L;
-            }
+        String passwordCifrado = DigestUtils.md5Hex(password);
+        Query q = em.createQuery("SELECT j FROM PgUsuario j WHERE j.estadoregistro = 1 AND j.usuario = :usuario AND j.password = :password");
+        q.setParameter("usuario", usuario);
+        q.setParameter("password", passwordCifrado);
+        if (q.getResultList().size() == 1) {
+            return 1L;
+        } else {
+            return 0L;
+        }
     }
-    
+
     public List<PgUsuario> selectUser(String usuario, String password) {
         String passwordCifrado = DigestUtils.md5Hex(password);
         //String passCifrado = c.cifrar(SeUsuario.getPassword());
         Query q = em.createQuery("SELECT j FROM PgUsuario j WHERE j.estadoregistro = 1 and j.usuario = :usuario and j.password = :password ");
-        q.setParameter("usuario", usuario);
+        q.setParameter("usuario", usuario.toLowerCase());
         q.setParameter("password", passwordCifrado);
         //System.out.println("password cifrado: "+passwordCifrado);
         return q.getResultList();
     }
-    
+
     public Long validPassword(String nombreUsuario, String passwordActual) {
         String passwordCifrado = DigestUtils.md5Hex(passwordActual);
         Query q = em.createQuery("SELECT j FROM PgUsuario j WHERE j.estadoregistro = 1 and j.usuario = :nombreUsuario AND j.password = :passwordCifrado ORDER BY j.id");
         q.setParameter("nombreUsuario", nombreUsuario);
         q.setParameter("passwordCifrado", passwordCifrado);
-        if (q.getResultList().size() == 1)
-            {
-                return 1L;
-            }
-            else
-            {
-                return 0L;
-            }
+        if (q.getResultList().size() == 1) {
+            return 1L;
+        } else {
+            return 0L;
+        }
     }
-    
+
     public List<PgUsuario> findUsuarioByID(Long id) {
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.estadoregistro = 1 and d.id = :id");
         q.setParameter("id", id);
         return q.getResultList();
     }
-    
-     public PgUsuario findRegistroUsuarioByID(Long id) {
+
+    public PgUsuario findRegistroUsuarioByID(Long id) {
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.estadoregistro = 1 and d.id = :id");
         q.setParameter("id", id);
         PgUsuario reg = (PgUsuario) q.getResultList();
         return reg;
     }
-       
+
     public List<PgUsuario> listaUsuario() {
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.estadoregistro = 1 order by d.usuario");
         //System.out.println("Size usuario: "+q.getResultList().size());
         ////System.out.println("Contenido dimensiones: "+q.getResultList());
         return q.getResultList();
-    }    
-    
+    }
+
     public List<PgUsuario> listaUsuarioSearch(String txt) {
         String txtUpper = txt.toUpperCase();
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.estadoregistro = 1 and UPPER(d.usuario) like :txt");
-        q.setParameter("txt", "%"+txtUpper+"%");
+        q.setParameter("txt", "%" + txtUpper + "%");
         ////System.out.println("Size dimensiones search: "+q.getResultList().size());
         ////System.out.println("Contenido dimensiones search: "+q.getResultList());
         return q.getResultList();
     }
-    
-     public void actualizarPgUsuario(PgUsuario PgUsuario) {
+
+    public void actualizarPgUsuario(PgUsuario PgUsuario) {
         em.merge(PgUsuario);
         em.flush();
     }
-       
+
     public void crearPgUsuario(PgUsuario PgUsuario) {
         em.persist(PgUsuario);
         em.flush();
@@ -127,37 +119,31 @@ public class UsuarioFacade extends AbstractFacade<PgUsuario> {
         em.merge(PgUsuario);
         em.flush();
     }
-       
+
     public List<PgUsuario> validarPgUsuario(PgUsuario PgUsuario) throws Exception {
-        System.out.println("Into facade login objeto : "+ PgUsuario.getUsuario());
+        System.out.println("Into facade login objeto : " + PgUsuario.getUsuario());
         String passCifrado = DigestUtils.md5Hex(PgUsuario.getPassword());
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.usuario = :usuario and d.password = :clave");
         q.setParameter("usuario", PgUsuario.getUsuario());
         q.setParameter("clave", passCifrado);
-        if (q.getResultList().size() == 1)
-        {
+        if (q.getResultList().size() == 1) {
             return q.getResultList();
-        }
-        else
-        {
+        } else {
             return null;
         }
     }
-    
+
     public Long validarUsuario(PgUsuario PgUsuario) throws Exception {
-        System.out.println("Into facade login Long : "+ PgUsuario.getUsuario());
-        System.out.println("Into facade login : "+ PgUsuario.getPassword());
+        System.out.println("Into facade login Long : " + PgUsuario.getUsuario());
+        System.out.println("Into facade login : " + PgUsuario.getPassword());
         String passCifrado = DigestUtils.md5Hex(PgUsuario.getPassword());
         //System.out.println("password cifrado : "+ passCifrado);
         Query q = em.createQuery("SELECT d FROM PgUsuario d WHERE d.usuario = ?1 and d.password = ?2");
         q.setParameter(1, PgUsuario.getUsuario());
         q.setParameter(2, passCifrado);
-        if (q.getResultList().size() == 1)
-        {
+        if (q.getResultList().size() == 1) {
             return 1L;
-        }
-        else
-        {
+        } else {
             return 0L;
         }
     }
